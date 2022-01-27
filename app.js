a36d88a4bc7d1b28424f15603bacbcf5b7471dfc
katskreativity <91428625+katskreativity@users.noreply.github.com>
2022-01-27 14:25:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+function submit()
+{
+    let uInput = document.getElementById("answer1").value;
+    let rError =document.getElementById("error1");
+
+    if(uInput == "34") {
+        window.open("test.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+function submit1()
+{
+    let uInput = document.getElementById("answer1").value;
+    let rError =document.getElementById("error1");
+
+    if(uInput == "0.4") {
+        window.open("q2.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit2()
+{
+    let uInput = document.getElementById("answer2").value;
+    let rError =document.getElementById("error2");
+
+    if(uInput == "-0.6") {
+        window.open("q3.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit3()
+{
+    let uInput = document.getElementById("answer3").value;
+    let rError =document.getElementById("error3");
+
+    if(uInput == "-52000") {
+        window.open("q4.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit4()
+{
+    let uInput = document.getElementById("answer4").value;
+    let rError =document.getElementById("error4");
+
+    if(uInput == "29") {
+        window.open("q5.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit5()
+{
+    let uInput = document.getElementById("answer5").value;
+    let rError =document.getElementById("error5");
+
+    if(uInput == "8.6") {
+        window.open("q6.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit6()
+{
+    let uInput = document.getElementById("answer6").value;
+    let rError =document.getElementById("error6");
+
+    if(uInput == "58.2") {
+        window.open("q7.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit7()
+{
+    let uInput = document.getElementById("answer7").value;
+    let rError =document.getElementById("error7");
+
+    if(uInput == "true, 1, -6") {
+        window.open("q8.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit8()
+{
+    let uInput = document.getElementById("answer7").value;
+    let rError =document.getElementById("error7");
+
+    if(uInput == "true, 1, -6") {
+        window.open("q8.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
+
+function submit9()
+{
+    let uInput = document.getElementById("answer9").value;
+    let rError =document.getElementById("error9");
+
+    if(uInput == "THANOS!!") {
+        window.open("final.html")
+    }
+    else {
+        rError.innerHTML = "Please try again. Your answer was incorrect.";
+    }
+    return true;
+}
